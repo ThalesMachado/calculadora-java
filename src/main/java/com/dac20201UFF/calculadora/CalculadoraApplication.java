c8eaa74bdd6b3c2fe75e3300b34f3d250b40a671
Thales Machado <thalesm@ipiranga.ipiranga>
2020-09-17 09:22:03 -0300

    Project Created with Spring Initializr

---
+++ src/main/java/com/dac20201UFF/calculadora/CalculadoraApplication.java
@@ -0,0 +1,13 @@
+package com.dac20201UFF.calculadora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculadoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculadoraApplication.class, args);
+	}
+
+}
